b4e5ee45c0aa7abeb2b9715e248f0161e549ab7c
hafizfadzil <hafizfadzil.gr@gmail.com>
2019-01-12 19:55:00 +0000

    Demo01

---
+++ ExplodingDice/Assets/Scripts/FollowMe.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMe : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    [SerializeField]
+    private float movespeed = 1f;
+
+    [HideInInspector]
+    public int waypointIndex = 0;
+
+    public bool moveAllowed = false;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (moveAllowed)
+            Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position,
+                movespeed * Time.deltaTime);
+
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+            
+    }
+
+
+
+}
