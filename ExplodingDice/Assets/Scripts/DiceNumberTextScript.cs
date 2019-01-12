05d4e49fa65e43c0144c7d79a5ac3ab02a0be5d7
hafizfadzil <hafizfadzil.gr@gmail.com>
2019-01-12 15:33:02 +0000

    Added Dice Scripts
    Dice Script
    Dice Number Text
    Dice Check Zone

---
+++ ExplodingDice/Assets/Scripts/DiceNumberTextScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DiceNumberTextScript : MonoBehaviour
+{
+
+    public TextMeshProUGUI textMesh;
+    public static int diceNumber;
+
+    // Use this for initialization
+    void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh.text = diceNumber.ToString();
+    }
+}
